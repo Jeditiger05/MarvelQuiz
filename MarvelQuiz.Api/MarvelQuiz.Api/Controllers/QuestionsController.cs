cc721d9d9a9788b0b18c0b8bf059c343c7329be5
Jeditiger05 <john@jedimedia.it>
2020-02-16 01:06:00 +1100

    Update

--- MarvelQuiz.Api/MarvelQuiz.Api/Controllers/QuestionsController.cs
+++ MarvelQuiz.Api/MarvelQuiz.Api/Controllers/QuestionsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MarvelQuiz.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,14 @@ namespace MarvelQuiz.Api.Controllers
     [ApiController]
     public class QuestionsController : ControllerBase
     {
+        public List<Question> GetQuestions()
+        {
+            List<Question> quiz = new List<Question>();
+            quiz.Add(new Question("What Color is the Incredible Hulk?", "Green", "Red", "Blue", "Yellow", "#answer1"));
+            quiz.Add(new Question("What does Captain America use to battle Villians?", "Hammer", "Sword", "Shield", "Blaster", "#answer3"));
+            quiz.Add(new Question("Who does Black Widow work for?", "Hydra", "Tony Stark", "F.B.I", "S.H.I.E.L.D", "#answer4"));
+
+            return quiz;
+        }
     }
 }
