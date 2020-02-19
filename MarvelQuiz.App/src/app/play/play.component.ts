0dc0d2f232569d88708cd038272bd1bf87deafe7
Jeditiger05 <john@jedimedia.it>
2020-02-19 12:46:28 +1100

    Added click event and answer working

--- MarvelQuiz.App/src/app/play/play.component.ts
+++ MarvelQuiz.App/src/app/play/play.component.ts
@@ -11,10 +11,19 @@ export class PlayComponent implements OnInit {
 
   public questionsList: QuestionResponceModel[];
   index: number = 0
+  answer: string;
 
   constructor(public questService: QuestionsService) { }
 
   ngOnInit() {
+  }
+
+  myAnswer(event) {
+    if (event.target.id == this.questionsList[this.index].correct)
+      this.answer = "Correct";
+    else
+      this.answer = "Wrong";
+    console.log(event.target.id);
   }
 
   onButtonClick() {
@@ -25,9 +34,10 @@ export class PlayComponent implements OnInit {
     });
   }
 
-  onButtonClickNext(){
+  onButtonClickNext() {
+    this.answer = "";
     this.index++;
-    if(this.index == this.questionsList.length)
-    this.index = 0;
+    if (this.index == this.questionsList.length)
+      this.index = 0;
   }
 }
